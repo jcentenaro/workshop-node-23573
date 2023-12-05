45b7055ddc2cb38f66761699618efb7659d94074
jcentenaro <info@pitzil.com>
2023-12-05 13:05:51 -0300

    agrergo layouts

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
+const expressLayouts = require("express-ejs-layouts");
 
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 app.set("views", "./src/views");
+
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout");
 
 app.use(require("./src/routes/tareasRouter"));
 
